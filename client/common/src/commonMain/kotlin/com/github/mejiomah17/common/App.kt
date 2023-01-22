1edb6d458c29817cdd885f28491550fd83c9e24f
mark <epshteinme@gmail.com>
2023-01-22 18:22:57 +0000

    Center root

--- client/common/src/commonMain/kotlin/com/github/mejiomah17/common/App.kt
+++ client/common/src/commonMain/kotlin/com/github/mejiomah17/common/App.kt
@@ -1,5 +1,7 @@
 package com.github.mejiomah17.common
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
@@ -7,15 +9,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
     val platformName = getPlatformName()
-
-    Button(onClick = {
-        text = "Hello, ${platformName}"
-    }) {
-        Text(text)
+    Box(modifier = Modifier.fillMaxSize()) {
+        Button(onClick = {
+            text = "Hello, ${platformName}"
+        }, modifier = Modifier.align(Alignment.Center)) {
+            Text(text)
+        }
     }
+
 }
