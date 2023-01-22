47ebf5671f3fbe94153dd3cbdc6448130b762a8c
mark <epshteinme@gmail.com>
2023-01-22 19:05:22 +0000

    Node editable in desktop

--- client/common/src/commonMain/kotlin/com/github/mejiomah17/common/App.kt
+++ client/common/src/commonMain/kotlin/com/github/mejiomah17/common/App.kt
@@ -10,26 +10,38 @@ import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.input.ImeAction
 
 @Composable
 fun App() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Node(modifier = Modifier.align(Alignment.Center))
+    val nodeState = remember { NodeState() }
+    Box(modifier = Modifier.fillMaxSize().pointerInput(Unit) {
+        detectTapGestures(
+            onTap = {
+                nodeState.editable = false
+            },
+        )
+    }) {
+        Node(nodeState = nodeState, modifier = Modifier.align(Alignment.Center))
     }
 }
 
+class NodeState() {
+    var editable by mutableStateOf(false)
+}
+
 @Composable
-fun Node(modifier: Modifier) {
+fun Node(nodeState: NodeState = remember { NodeState() }, modifier: Modifier) = nodeState.run {
     var text by remember { mutableStateOf("Hello, World!") }
-    var editable by remember { mutableStateOf(false) }
     if (!editable) {
         hideKeyboard()
     }
@@ -41,12 +53,24 @@ fun Node(modifier: Modifier) {
             detectTapGestures(
                 onLongPress = {
                     editable = true
-                }
+                },
             )
         },
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
         keyboardActions = KeyboardActions(
             onDone = {
+                editable = false
+            },
+            onGo = {
+                editable = false
+            },
+            onNext = {
+                editable = false
+            },
+            onPrevious = {
+                editable = false
+            },
+            onSend = {
                 editable = false
             },
         )
